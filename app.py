d9087ad03589cfc0a62d404558333890025a30cd
hugooleal <77586151+hugooleal@users.noreply.github.com>
2024-05-10 23:39:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-import json
-import os
-import requests
-from flask import Flask, request, Response, jsonify
+from flask import Flask, request
 from flask_cors import CORS
 import google.generativeai as genai
 
@@ -14,15 +11,15 @@ genai.configure(api_key="AIzaSyACn0ut4jxm-Kp9N5zcuOYQrTRJe6Z6aPQ")
 # Set up the model
 generation_config = {
     "temperature": 0.5,
-    "top_p": 1,
-    "top_k": 0,
-    "max_output_tokens": 2048,
 }
 
 safety_settings = [
     {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
     {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+    },
     {
         "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
         "threshold": "BLOCK_MEDIUM_AND_ABOVE",
@@ -40,14 +37,13 @@ model = genai.GenerativeModel(
 def post_example():
     if request.method == "POST":
         # Access POST data from the request
-        rawPrompt = request.get_json()["prompt"]
-        formattedPrompt = f"{rawPrompt}"
-        print("Prompt:", formattedPrompt)
+        prompt = request.get_json()["prompt"]
+        print("Prompt:", f"{prompt}")
 
         # Trying to parse message
         try:
 
-            response = model.generate_content(formattedPrompt)
+            response = model.generate_content(prompt)
 
             print(response.text)
 
