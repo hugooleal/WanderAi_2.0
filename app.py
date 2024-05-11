1212dacfd55adfb969bcd64e07740bef06311714
hugooleal <77586151+hugooleal@users.noreply.github.com>
2024-05-11 00:31:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ genai.configure(api_key="AIzaSyACn0ut4jxm-Kp9N5zcuOYQrTRJe6Z6aPQ")
 # Set up the model
 generation_config = {
     "temperature": 0.5,
+    "top_p": 0.95,
+    "top_k": 0,
+    "max_output_tokens": 8192,
 }
 
 safety_settings = [
@@ -18,7 +21,7 @@ safety_settings = [
     {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
     {
         "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        "threshold": "BLOCK_NONE",
     },
     {
         "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
@@ -27,7 +30,7 @@ safety_settings = [
 ]
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.0-pro",
+    model_name="gemini-1.5-pro-latest",
     generation_config=generation_config,
     safety_settings=safety_settings,
 )
